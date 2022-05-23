ca3c287534c95ce1faf258386d4911553cada344
Zake-Ahmed <zake3012@gmail.com>
2022-05-23 14:41:02 +0100

    Items file added

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,82 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+	
+	private long id;
+	private String productName;
+	private float price;
+
+	public Item(String productName, float price) {
+		super();
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public Item(long id, String productName, float price) {
+		super();
+		this.id = id;
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", productName=" + productName + ", price=" + price + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + Float.floatToIntBits(price);
+		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		if (Float.floatToIntBits(price) != Float.floatToIntBits(other.price))
+			return false;
+		if (productName == null) {
+			if (other.productName != null)
+				return false;
+		} else if (!productName.equals(other.productName))
+			return false;
+		return true;
+	}
+
+}
